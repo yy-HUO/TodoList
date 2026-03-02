4d4bde10bf19206c1422bf906ab1cc0776d262b4
yy-HUO <465758923@qq.com>
2026-03-03 05:28:59 +0800

    small Mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import TodoInput from "./components/TodoInput.jsx";
-import TodoList from "./components/TodoList.jsx";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
